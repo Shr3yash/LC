b2b6bd16791bf602542d0d03144e4684e1b8e999
Shreyash <60093808+DrCybernotix@users.noreply.github.com>
2021-12-24 14:47:17 +0530

    Update Container With Most Water.cpp

--- Container With Most Water.cpp
+++ Container With Most Water.cpp
@@ -1,4 +1,5 @@
 //   TLE (54/60)
+//   O(n^2) time, O(1) space
 class Solution {
 public:
     int maxArea(vector<int>& height) {
@@ -10,4 +11,23 @@ public:
         }
         return maxAreaa;
     }
+};
+
+
+//  optimized runs
+//  O(n) time, O(n) space
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int maxAreaa=0,area=0,i=0,j=height.size()-1;
+        while(i<j){
+            area=min(height[i],height[j])*(j-i);
+            maxAreaa=max(maxAreaa,area);
+            if(height[i]<=height[j]){i++;}
+            else
+                j--;
+        }        
+        return maxAreaa;
+    }  
 };
