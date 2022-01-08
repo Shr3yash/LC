be9300cd8b8b8a08d02a230035ab38fdcb9c34f3
Shreyash <60093808+DrCybernotix@users.noreply.github.com>
2022-01-08 12:53:37 +0530

    Create Trapping Rain Water.cpp

---
+++ Trapping Rain Water.cpp
@@ -0,0 +1,33 @@
+//TLE
+
+class Solution 
+{
+public:
+    int trap(vector<int>& height) 
+    {
+        int water=0;
+        vector<int>::iterator it;
+        for(int i=0; i<height.size(); i++)
+        {
+            int ptrLeft=i, ptrRight=i, maxLeft=0, maxRight=0;
+            while(ptrLeft>=0)
+            {
+                maxLeft=max(maxLeft,height[ptrLeft]);
+                ptrLeft--;
+            }
+            while(ptrRight<height.size())
+            {
+                maxRight=max(maxRight,height[ptrRight]);
+                ptrRight++;
+            }
+            
+            int currentWater=min(maxLeft,maxRight)-height[i];
+            
+            if(currentWater>=0)
+            {
+                water+=currentWater;
+            } 
+        }
+        return water;
+    }
+};
