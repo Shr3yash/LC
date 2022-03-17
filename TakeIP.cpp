de499af1d48c134fee6c356541193f4ae246252f
Shreyash <60093808+Shr3yash@users.noreply.github.com>
2022-03-17 15:12:41 +0530

    Create TakeIP.cpp

---
+++ TakeIP.cpp
@@ -0,0 +1,11 @@
+//of dyna2D ntom
+cin>>n>>m;
+int **dpr =  new int * demo[n]    //hors
+  
+for (int i = 0; i < n; i++) {
+  demo[i] =  new int[n];          //eles
+    for (int i = 0; i < m; i++) {
+      cin>>demo[i][j];
+    }
+}
+    
