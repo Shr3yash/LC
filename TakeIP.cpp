745da795da21f6fc3cf6e65d09132147021b5fdb
Shreyash <60093808+Shr3yash@users.noreply.github.com>
2022-03-17 16:56:05 +0530

    Update TakeIP.cpp

--- TakeIP.cpp
+++ TakeIP.cpp
@@ -1,11 +1,21 @@
-//of dyna2D ntom
+//of dyna2D mton
 cin>>n>>m;
-int **dpr =  new int * demo[n]    //hors
+int **dpr =  new int * demo[n]    // r=ws
   
 for (int i = 0; i < n; i++) {
-  demo[i] =  new int[n];          //eles
+  demo[i] =  new int[n];          //eles||hors
     for (int i = 0; i < m; i++) {
       cin>>demo[i][j];
     }
 }
     
+// delete [] demo; //f11D
+
+if(int i=0; i<n; i++){
+  delete [] demo[i];
+}
+delete [] demo;
+
+
+
+
