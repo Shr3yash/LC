d4770c9bdb7d8e5a8acfdcce78e1b9ee539e1747
Shreyash <60093808+Shr3yash@users.noreply.github.com>
2022-03-20 04:59:38 +0530

    Update Is Subsequence.cpp

--- Is Subsequence.cpp
+++ Is Subsequence.cpp
@@ -9,3 +9,26 @@ public:
         
     }
 };
+
+//Wrecursion
+
+class Solution {
+public:
+    bool recur(string& x, string& y, int n, int m)
+    {
+        if(n==0)
+            return true;
+        if(m==0)
+            return false;
+        if(x[n-1]==y[m-1])
+            return recur(x,y,n-1,m-1);
+        return recur(x,y,n,m-1);
+    }
+    bool isSubsequence(string s, string t) {
+
+        if(recur(s,t,s.length(),t.length()))
+            return true;
+        return false;
+        
+    }
+};
