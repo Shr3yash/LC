4490bba5bc5fb7d82d7619ddf8ab826ee0f17be8
Shreyash <60093808+Shr3yash@users.noreply.github.com>
2022-03-22 04:25:33 +0530

    Update Permutations.cpp

--- Permutations.cpp
+++ Permutations.cpp
@@ -30,3 +30,30 @@ public:
         return ans;
     }
 };
+
+
+//flashbang
+
+class Solution {
+    
+public:
+    void perm(int index, vector<int>& nums, vector<vector<int>>& ans){
+        if(index==nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+        for(int i =index; i<nums.size(); i++){
+            swap(nums[index], nums[i]);
+            perm(index+1, nums, ans);
+            swap(nums[index], nums[i]);
+            }
+        }
+        
+    
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;  
+        perm(0, nums, ans);
+        return ans;
+    }
+};
