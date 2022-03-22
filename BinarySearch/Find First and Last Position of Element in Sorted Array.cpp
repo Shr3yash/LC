e189804dc07761ef2a89316d90212b522007c55e
Shreyash <60093808+Shr3yash@users.noreply.github.com>
2022-03-23 04:29:37 +0530

    Update Find First and Last Position of Element in Sorted Array.cpp

--- BinarySearch/Find First and Last Position of Element in Sorted Array.cpp
+++ BinarySearch/Find First and Last Position of Element in Sorted Array.cpp
@@ -30,3 +30,62 @@ public:
         return ans; 
     }
 };
+
+//TC=logn
+class Solution {
+public:
+ int firstOcc(vector<int>& arr, int n, int key) {
+
+    int s = 0, e = n-1;
+    int mid = s + (e-s)/2;
+    int ans = -1;
+    while(s<=e) {
+
+        if(arr[mid] == key){
+            ans = mid;
+            e = mid - 1;
+        }
+        else if(key > arr[mid]) {//Right me jao
+            s = mid + 1;
+        }
+        else if(key < arr[mid]) {//left me jao
+            e = mid - 1;
+        }
+
+        mid = s + (e-s)/2;
+    }
+    return ans;
+}
+
+int lastOcc(vector<int>& arr, int n, int key) {
+
+    int s = 0, e = n-1;
+    int mid = s + (e-s)/2;
+    int ans = -1;
+    while(s<=e) {
+
+        if(arr[mid] == key){
+            ans = mid;
+            s = mid + 1;
+        }
+        else if(key > arr[mid]) {//Right me jao
+            s = mid + 1;
+        }
+        else if(key < arr[mid]) {//left me jao
+            e = mid - 1;
+        }
+
+        mid = s + (e-s)/2;
+    }
+    return ans;
+}
+
+public:
+    vector<int> searchRange(vector<int>& arr, int key) {
+        vector<int> ans;
+        int n=arr.size();
+        ans.push_back(firstOcc(arr, n, key));
+        ans.push_back(lastOcc(arr, n, key));
+        return ans; 
+    }
+};
