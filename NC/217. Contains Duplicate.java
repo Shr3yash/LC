dac71b48227e0b1eb571d5e3fd81d8d657e331be
Shreyash <12shreyashh@gmail.com>
2026-01-19 08:49:54 -0600

    Refactor hasDuplicate to use HashSet for duplicates
    Refactored hasDuplicate method to use a HashSet for O(n) time complexity.

--- NC/217. Contains Duplicate.java
+++ NC/217. Contains Duplicate.java
@@ -1,4 +1,26 @@
 class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        // for(int i = 0; i < nums.length; i++){
+        //     for(int j = i+1; j<nums.length; j++){
+        //         if(nums[i] == nums[j])
+        //             return true;
+        //     }
+        // }
+        // return false;
+        //n2, 1
+        Set<Integer> hashmap = new HashSet<>();
+        for(int n = 0; n<nums.length; n++){
+            if(hashmap.contains(nums[n])){
+                return true;
+            }
+            hashmap.add(nums[n]);
+        }
+        return false;
+        //n, n
+    }
+}
+
+class Solution {
     public boolean containsDuplicate(int[] nums) {
         HashMap <Integer, Integer> zero = new HashMap<>();
         for(int num: nums){
